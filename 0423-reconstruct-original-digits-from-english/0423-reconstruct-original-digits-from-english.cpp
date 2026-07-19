3f62c545e4dd352301b87d56dd18cea3a8408cd2
Shafiullah Raihan <140353900+Shafiullah-Raihan@users.noreply.github.com>
2026-07-19 22:14:07 +0600

    Time: 0 ms (100%), Space: 12.1 MB (54.98%) - LeetHub

---
+++ 0423-reconstruct-original-digits-from-english/0423-reconstruct-original-digits-from-english.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    string originalDigits(string s) {
+
+        vector<int> freq(26, 0);
+        for (char c : s) {
+            freq[c - 'a']++;
+        }
+
+        vector<int> count(10, 0);
+
+        count[0] = freq['z' - 'a']; 
+        count[2] = freq['w' - 'a']; 
+        count[4] = freq['u' - 'a']; 
+        count[6] = freq['x' - 'a']; 
+        count[8] = freq['g' - 'a']; 
+
+        count[3] = freq['h' - 'a'] - count[8];                 // three
+        count[5] = freq['f' - 'a'] - count[4];                 // five
+        count[7] = freq['s' - 'a'] - count[6];                 // seven
+        count[1] = freq['o' - 'a'] - count[0] - count[2] - count[4]; // one
+        count[9] = freq['i' - 'a'] - count[5] - count[6] - count[8]; // nine
+
+        string result;
+        for (int d = 0; d <= 9; ++d) {
+            result.append(count[d], char('0' + d));
+        }
+
+        return result;
+    }
+};
