62da452aeeb165ede7eaf4d743cfb1b28633696b
Shafiullah Raihan <140353900+Shafiullah-Raihan@users.noreply.github.com>
2026-08-27 10:55:01 +0600

    Time: 39 ms (67.13%), Space: 62.4 MB (96.73%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+
+    void raihan(int n,int k,vector<vector<int>>&ans,vector<int>& temp,int idx)
+    {   
+        // if(idx>n) return ;
+        if(temp.size() == k) 
+        {
+            ans.push_back(temp);return;
+        }
+        for(int i=idx;i<=n;i++)
+        {
+            
+                temp.push_back(i);
+            
+            raihan(n,k,ans,temp,i+1);
+            temp.pop_back();
+        }
+
+    }
+
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>>ans;
+        vector<int>temp;
+        raihan(n,k,ans,temp,1);
+        return ans;
+    }
+};
