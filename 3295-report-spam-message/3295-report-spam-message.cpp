da9c1a1cdf8258fe3b76b1c39e4da415a6f04f78
Shafiullah Raihan <140353900+Shafiullah-Raihan@users.noreply.github.com>
2026-09-05 21:39:10 +0600

    Time: 610 ms (5.24%), Space: 291.1 MB (31.48%) - LeetHub

---
+++ 3295-report-spam-message/3295-report-spam-message.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool reportSpam(vector<string>& message, vector<string>& bannedWords) {
+        set<string>st;
+        for(auto u:bannedWords)
+        {
+            st.insert(u);
+        }
+        int cnt=0;
+        for(auto u:message)
+        {
+            if(st.count(u)) cnt++;
+        }
+        if(cnt>=2)
+        {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
